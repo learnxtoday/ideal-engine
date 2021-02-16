b0c9e4e46e782aac2989bc3cde10a68559f00ce4
Parth Krishna <thenerdsuperuser@gmail.com>
2021-02-16 22:06:08 +0530

    Need to update setup.py for PyPi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
         name="Ideal Engine",
-        version="1.0.1",
+        version="1.0.2",
         author="Parth Krishna",
         author_email="thenerdsuperuser@gmail.com",
         description="A small project management tool",
